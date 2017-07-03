aaf9b46d811f2ef6cf19d0c9ac4263d0f93ab2ea
MACK Mathieu <mathieu.mack@econocom.com>
2017-07-03 09:23:08 +0200

    Adding ContextReplacement on Fontcolor property for Labels
    Issue #42

--- MvvX.Plugins.Open-XML-SDK/MvvX.Plugins.Open-XML-SDK/Word/ReportEngine/BatchModels/ContextModel.cs
+++ MvvX.Plugins.Open-XML-SDK/MvvX.Plugins.Open-XML-SDK/Word/ReportEngine/BatchModels/ContextModel.cs
@@ -116,13 +116,15 @@ namespace MvvX.Plugins.OpenXMLSDK.Word.ReportEngine.BatchModels
         }
 
         /// <summary>
-        /// Replace text and visibility of element
+        /// Replace text, FontColor and visibility of element
         /// </summary>
         /// <param name="element"></param>
         public void ReplaceItem(Label element)
         {
             if(!string.IsNullOrEmpty(element.Text))
                 element.Text = ReplaceText(element.Text);
+            if (!string.IsNullOrEmpty(element.FontColor))
+                element.FontColor = ReplaceText(element.FontColor);
             SetVisibilityFromContext(element);
         }
 
