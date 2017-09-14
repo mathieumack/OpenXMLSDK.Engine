7d34a447456ac41bf4e4e40273e681e733cde12c
AmrouOUNI <amrou.ouni@econocom.com>
2017-09-14 09:50:27 +0200

    fix margin changes when append document (#97)
    #95

--- MvvX.Plugins.Open-XML-SDK/MvvX.Plugins.Open-XML-SDK.Shared/Word/WordManager.cs
+++ MvvX.Plugins.Open-XML-SDK/MvvX.Plugins.Open-XML-SDK.Shared/Word/WordManager.cs
@@ -438,10 +438,10 @@ namespace MvvX.Plugins.OpenXMLSDK.Platform.Word
 
             AltChunk altChunk = new AltChunk();
             altChunk.Id = wdMainDocumentPart.GetIdOfPart(formatImportPart);
-
-            var lastElement = wdMainDocumentPart.Document.Body.Elements().Last(x => (x.GetType() == typeof(Paragraph)) || (x.GetType() == typeof(SectionProperties)));
+            
+            var lastElement = wdMainDocumentPart.Document.Body.Elements().Last(x => x.GetType() == typeof(Paragraph) || x.GetType() == typeof(SectionProperties)|| x.GetType() == typeof(AltChunk));
             lastElement.InsertAfterSelf(altChunk);
-            if (withPageBreak && lastElement is Paragraph)
+            if (withPageBreak && (lastElement is Paragraph|| lastElement is AltChunk))
             {
                 Paragraph p = new Paragraph(
                     new Run(
