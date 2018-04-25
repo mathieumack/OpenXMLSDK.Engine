85ab6c6fe021b5deded204b772c2d6844e201062
MACK Mathieu <mathieu.mack@econocom.com>
2018-04-26 01:07:15 +0200

    Try to reduce complexity

--- MvvX.Plugins.Open-XML-SDK/MvvX.Plugins.Open-XML-SDK.Shared/Word/ReportEngine/BaseElementExtensions.cs
+++ MvvX.Plugins.Open-XML-SDK/MvvX.Plugins.Open-XML-SDK.Shared/Word/ReportEngine/BaseElementExtensions.cs
@@ -24,61 +24,70 @@ namespace MvvX.Plugins.OpenXMLSDK.Platform.Word.ReportEngine
 
             if (element.Show)
             {
-                // Keep this statement order, because of the UniformGrid inherits from Table
-                if (element is ForEach)
-                {
-                    (element as ForEach).Render(parent, context, documentPart, formatProvider);
-                }
-                if (element is Label)
-                {
-                    createdElement = (element as Label).Render(parent, context, documentPart, formatProvider);
-                }
-                else if (element is BookmarkStart)
-                {
-                    createdElement = (element as BookmarkStart).Render(parent, context, formatProvider);
-                }
-                else if (element is BookmarkEnd)
-                {
-                    createdElement = (element as BookmarkEnd).Render(parent, context, formatProvider);
-                }
-                else if (element is Hyperlink)
-                {
-                    createdElement = (element as Hyperlink).Render(parent, context, documentPart, formatProvider);
-                }
-                else if (element is Paragraph)
-                {
-                    createdElement = (element as Paragraph).Render(parent, context, formatProvider);
-                }
-                else if (element is Image)
-                {
-                    createdElement = (element as Image).Render(parent, context, documentPart);
-                }
-                else if (element is UniformGrid)
-                {
-                    createdElement = (element as UniformGrid).Render(parent, context, documentPart, formatProvider);
-                }
-                else if (element is Table)
-                {
-                    createdElement = (element as Table).Render(parent, context, documentPart, formatProvider);
-                }
-                else if (element is TableOfContents)
-                {
-                    (element as TableOfContents).Render(documentPart, context);
-                }
-                else if (element is BarModel)
-                {
-                    (element as BarModel).Render(parent, context, documentPart, formatProvider);
-                }
+                createdElement = element.RenderItem(parent, context, documentPart, formatProvider);
+            }
+            return createdElement;
+        }
 
-                if (element.ChildElements != null && element.ChildElements.Count > 0)
+        private static OpenXmlElement RenderItem(this BaseElement element, OpenXmlElement parent, ContextModel context, OpenXmlPart documentPart, IFormatProvider formatProvider)
+        {
+            OpenXmlElement createdElement = null;
+
+            // Keep this statement order, because of the UniformGrid inherits from Table
+            if (element is ForEach)
+            {
+                (element as ForEach).Render(parent, context, documentPart, formatProvider);
+            }
+            else if (element is Label)
+            {
+                createdElement = (element as Label).Render(parent, context, documentPart, formatProvider);
+            }
+            else if (element is BookmarkStart)
+            {
+                createdElement = (element as BookmarkStart).Render(parent, context, formatProvider);
+            }
+            else if (element is BookmarkEnd)
+            {
+                createdElement = (element as BookmarkEnd).Render(parent, context, formatProvider);
+            }
+            else if (element is Hyperlink)
+            {
+                createdElement = (element as Hyperlink).Render(parent, context, documentPart, formatProvider);
+            }
+            else if (element is Paragraph)
+            {
+                createdElement = (element as Paragraph).Render(parent, context, formatProvider);
+            }
+            else if (element is Image)
+            {
+                createdElement = (element as Image).Render(parent, context, documentPart);
+            }
+            else if (element is UniformGrid)
+            {
+                createdElement = (element as UniformGrid).Render(parent, context, documentPart, formatProvider);
+            }
+            else if (element is Table)
+            {
+                createdElement = (element as Table).Render(parent, context, documentPart, formatProvider);
+            }
+            else if (element is TableOfContents)
+            {
+                (element as TableOfContents).Render(documentPart, context);
+            }
+            else if (element is BarModel)
+            {
+                (element as BarModel).Render(parent, context, documentPart, formatProvider);
+            }
+
+            if (element.ChildElements != null && element.ChildElements.Count > 0)
+            {
+                foreach (var e in element.ChildElements)
                 {
-                    foreach (var e in element.ChildElements)
-                    {
-                        e.InheritFromParent(element);
-                        e.Render(createdElement ?? parent, context, documentPart, formatProvider);
-                    }
+                    e.InheritFromParent(element);
+                    e.Render(createdElement ?? parent, context, documentPart, formatProvider);
                 }
             }
+
             return createdElement;
         }
     }
