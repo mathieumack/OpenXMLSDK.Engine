f6539c6c8871c5ad9c042b91c516c13514d47e10
Alexandre bonnet <alexandre.bonnet@econocom.com>
2019-05-23 15:36:06 +0200

    Add the "Can't split" property on the UniformGrid for all rows

--- MvvX.Plugins.Open-XML-SDK/MvvX.Plugins.Open-XML-SDK/Word/ReportEngine/Models/UniformGrid.cs
+++ MvvX.Plugins.Open-XML-SDK/MvvX.Plugins.Open-XML-SDK/Word/ReportEngine/Models/UniformGrid.cs
@@ -1,16 +1,24 @@
-﻿using MvvX.Plugins.OpenXMLSDK.Word.ReportEngine.Models.Attributes;
-using MvvX.Plugins.OpenXMLSDK.Word.Tables.Models;
-
-namespace MvvX.Plugins.OpenXMLSDK.Word.ReportEngine.Models
+﻿namespace MvvX.Plugins.OpenXMLSDK.Word.ReportEngine.Models
 {
+    /// <summary>
+    /// Uniform Grid
+    /// </summary>
     public class UniformGrid : Table
     {
+        /// <summary>
+        /// Model of each cell
+        /// </summary>
         public Cell CellModel { get; set; }
+
+        /// <summary>
+        /// Indicate if rows can be splited in multiple pages
+        /// </summary>
+        public bool CantSplitRows { get; set; }
 
         /// <summary>
         /// Constructor
         /// </summary>
-        public UniformGrid() 
+        public UniformGrid()
             : base(typeof(UniformGrid).Name)
         {
         }
