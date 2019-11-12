0d06311db21a647bd3fda01a7c84022c3b31008b
SANHNV <45290025+SANHNV@users.noreply.github.com>
2019-11-12 11:26:03 +0100

    test marion

--- OpenXMLSDK.UnitTest/ReportEngine/ReportEngineUnitTests.cs
+++ OpenXMLSDK.UnitTest/ReportEngine/ReportEngineUnitTests.cs
@@ -1,6 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenXMLSDK.Engine.Word;
 using OpenXMLSDK.Engine.Word.Models;
+using OpenXMLSDK.Engine.Word.Charts;
+using OpenXMLSDK.Engine.Word.Paragraphs.Models;
+using OpenXMLSDK.Engine.Word.Tables.Models;
 
 namespace OpenXMLSDK.UnitTest.ReportEngine
 {
@@ -66,14 +69,90 @@ namespace OpenXMLSDK.UnitTest.ReportEngine
             testSM.Fill = "color";
             testSM.ThemeShade = "toto";
             testSM.ThemeTint = "toto";
-            //testSM.Val = 0;
+            testSM.Val = (ShadingPatternValues)0;
             testSM.ThemeColor = (ThemeColorValues)0;
             testSM.ThemeFill = (ThemeColorValues)0;
             Assert.AreEqual(testSM.ThemeFillShade, testSM.ThemeFillTint);
             Assert.AreEqual(testSM.Color, testSM.Fill);
             Assert.AreEqual(testSM.ThemeShade, testSM.ThemeTint);
             Assert.AreEqual(testSM.ThemeColor, testSM.ThemeFill);
-            //Assert.AreEqual(0, testSM.ThemeFill);
+            Assert.AreEqual((ShadingPatternValues)0, testSM.Val);
+        }
+
+        [TestMethod]
+        public void Test_Model_SpacingBetweenLinesModel()
+        {
+            SpacingBetweenLinesModel testSBLM = new SpacingBetweenLinesModel();
+            testSBLM.After = "toto";
+            testSBLM.Before = "toto";
+            Assert.AreEqual(testSBLM.After, testSBLM.Before);
+        }
+
+        [TestMethod]
+        public void Test_Charts()
+        {
+            ChartModelException testCharts = new ChartModelException("error");
+            ChartModelException testCharts2 = new ChartModelException("message","error");
+            ChartModelException testCharts3 = new ChartModelException("message", "error", null);
+            Assert.AreEqual("error", testCharts.ErrorCode);
+            Assert.IsNull(testCharts.InnerException);
+        }
+
+        [TestMethod]
+        public void Test_Paragraphs_BM()
+        {
+            ParagraphBorderModel testPBM = new ParagraphBorderModel();
+            Assert.AreEqual("FFFFFF", testPBM.Color);
+            Assert.IsNull(testPBM.Size);
+            Assert.AreEqual(BorderValues.None, testPBM.BorderValue);
+        }
+
+        [TestMethod]
+        public void Test_Paragraphs_BMs()
+        {
+            ParagraphBordersModel testPBMs = new ParagraphBordersModel();
+            testPBMs.BottomBorder = new ParagraphBorderModel();
+            testPBMs.TopBorder = new ParagraphBorderModel();
+            Assert.AreEqual(testPBMs.TopBorder.Color, testPBMs.BottomBorder.Color);
+            Assert.AreEqual(testPBMs.TopBorder.Size, testPBMs.BottomBorder.Size);
+            Assert.AreEqual(testPBMs.TopBorder.BorderValue, testPBMs.BottomBorder.BorderValue);
+
+            testPBMs.LeftBorder = new ParagraphBorderModel();
+            testPBMs.RightBorder = new ParagraphBorderModel();
+            Assert.AreEqual(testPBMs.LeftBorder.Color, testPBMs.RightBorder.Color);
+            Assert.AreEqual(testPBMs.LeftBorder.Size, testPBMs.RightBorder.Size);
+            Assert.AreEqual(testPBMs.LeftBorder.BorderValue, testPBMs.RightBorder.BorderValue);
+        }
+
+        [TestMethod]
+        public void Test_Paragraphs_PM()
+        {
+            ParagraphPropertiesModel testPM = new ParagraphPropertiesModel();
+            testPM.NumberingProperties = new NumberingPropertiesModel();
+            testPM.NumberingProperties.NumberingLevelReference = 1;
+            testPM.NumberingProperties.NumberingId = 1;
+            Assert.AreEqual(testPM.NumberingProperties.NumberingId, testPM.NumberingProperties.NumberingLevelReference);
+
+            testPM.ParagraphStyleId = new ParagraphStyleIdModel();
+            testPM.ParagraphStyleId.Val = "toto";
+            Assert.AreEqual("toto", testPM.ParagraphStyleId.Val);
+
+            testPM.SpacingBetweenLines = new SpacingBetweenLinesModel();
+            testPM.SpacingBetweenLines.After = "toto";
+            testPM.SpacingBetweenLines.Before = "toto";
+            Assert.AreEqual(testPM.SpacingBetweenLines.After, testPM.SpacingBetweenLines.Before);
+
+            testPM.ParagraphBorders = new ParagraphBordersModel();
+            //Assert à mutualiser
+        }
+
+        [TestMethod]
+        public void Test_Table()
+        {
+            TableBorderModel testTBM = new TableBorderModel();
+            Assert.AreEqual(BorderValues.Single, testTBM.BorderValue);
+            Assert.AreEqual(1, testTBM.Size);
+            Assert.AreEqual("000000", testTBM.Color);
         }
     }
 }
