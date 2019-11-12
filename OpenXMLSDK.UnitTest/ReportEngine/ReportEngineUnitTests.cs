97b446e02f039b633494e352a88eb3d2de20604a
manfredhq <38951963+manfredhq@users.noreply.github.com>
2019-11-12 09:29:26 +0100

    update unit test

--- OpenXMLSDK.UnitTest/ReportEngine/ReportEngineUnitTests.cs
+++ OpenXMLSDK.UnitTest/ReportEngine/ReportEngineUnitTests.cs
@@ -1,4 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenXMLSDK.Engine.Word;
+using OpenXMLSDK.Engine;
+using Moq;
+using System.IO;
+using System;
 
 namespace OpenXMLSDK.UnitTest.ReportEngine
 {
@@ -9,6 +14,60 @@ namespace OpenXMLSDK.UnitTest.ReportEngine
         public void Global_Generation()
         {
             ReportEngineTest.ReportEngine(string.Empty, string.Empty);
+        }
+
+        [TestMethod]
+        public void WordManagerOpenDocException()
+        {
+            WordManager manager = new WordManager();
+            Exception expectedExcetpion = null;
+
+            try
+            {
+                manager.OpenDoc("c", true);
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void WordManagerOpenDocFromTemplateException()
+        {
+            WordManager manager = new WordManager();
+            Exception expectedExcetpion = null;
+
+            try
+            {
+                manager.OpenDocFromTemplate("c");
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void WordManagerFindBookmarkException()
+        {
+            WordManager manager = new WordManager();
+            Exception expectedExcetpion = null;
+
+            try
+            {
+                manager.FindBookmark("c");
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            Assert.IsNotNull(expectedExcetpion);
         }
     }
 }
