c2806d73b4e7c818524148f672a54815947e4c77
nicolas CHAPELIER <nicolas.chapelier@econocom.com>
2020-03-27 11:09:05 +0100

    use return null

--- OpenXMLSDK.Engine/Word/ReportEngine/Renders/CellExtensions.cs
+++ OpenXMLSDK.Engine/Word/ReportEngine/Renders/CellExtensions.cs
@@ -1,12 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Globalization;
+using System.Linq;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
+using OpenXMLSDK.Engine.Platform.Word.Extensions;
 using OpenXMLSDK.Engine.ReportEngine.DataContext;
 using OpenXMLSDK.Engine.Word.ReportEngine.Models;
-using System.Globalization;
-using OpenXMLSDK.Engine.Platform.Word.Extensions;
-using System;
 
 namespace OpenXMLSDK.Engine.Word.ReportEngine.Renders
 {
@@ -70,7 +70,7 @@ namespace OpenXMLSDK.Engine.Word.ReportEngine.Renders
             }
 
             if (!cell.Show)
-                return wordCell;
+                return null;
 
             if (cell.ChildElements.Any(x => x is Models.TemplateModel))
             {
@@ -134,7 +134,7 @@ namespace OpenXMLSDK.Engine.Word.ReportEngine.Renders
                     element.InheritFromParent(cell);
                     element.Render(document, r, context, documentPart, formatProvider);
                 }
-            }      
+            }
 
             return wordCell;
         }
