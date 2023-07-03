94f7a23b3fbfa38d3b51048e72f5497ade484139
Mathieu Mack <mathieu.mack@outlook.fr>
2023-07-03 09:48:55 +0200

    Adding some unit tests to support full in memory process

--- src/OpenXMLSDK.UnitTest/ReportEngine/ReportEngineUnitTests.cs
+++ src/OpenXMLSDK.UnitTest/ReportEngine/ReportEngineUnitTests.cs
@@ -1,4 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Globalization;
+using OpenXMLSDK.Engine.Word.ReportEngine;
+using OpenXMLSDK.Engine.Word;
 
 namespace OpenXMLSDK.UnitTest.ReportEngine
 {
@@ -9,6 +15,39 @@ namespace OpenXMLSDK.UnitTest.ReportEngine
         public void Global_Generation()
         {
             ReportEngineTest.ReportEngine(string.Empty, string.Empty, false);
+        }
+
+        [TestMethod]
+        public void Global_Generation_Stream()
+        {
+            // Configuration of sample data :
+            var rootFolder = Path.Combine(Environment.CurrentDirectory, "Resources/MdFiles");
+            var templatePath = Path.Combine(Environment.CurrentDirectory, "Resources/Dotx/sample.dotx");
+            var outputPath = Path.Combine(Environment.CurrentDirectory, "Resources/Dotx/sample.docx");
+
+            // Generate Word document :
+            using var templateStream = File.OpenRead(templatePath);
+            var markdownContent = File.ReadAllText(Path.Combine(rootFolder, "0.md"));
+
+            // Launch transformation :
+            var reports = new List<Report>();
+
+            var culture = new CultureInfo("en-US");
+            using var templateDocument = File.OpenRead(templatePath);
+
+            var output = Stream.Null;
+            using (var word = new WordManager())
+            {
+                if (templateDocument != null && templateDocument != Stream.Null)
+                    word.OpenDocFromTemplate(templateDocument);
+
+                // Append documentation :
+                word.AppendSubDocument(reports, true, culture);
+
+                word.SaveDoc();
+
+                output = word.GetMemoryStream();
+            }
         }
     }
 }
